d5f4b1f1c11f18031f871d93e7f1882d091a9744
Nuno Ramos <Nuno.Ramos@ctw.bmwgroup.com>
2020-07-13 16:35:51 +0100

    diferent port

--- run_proj.sh
+++ run_proj.sh
@@ -2,4 +2,4 @@ mvn clean package
 
 docker build -t springio/gs-spring-boot-docker .
 
-docker run -p 9090:8000 springio/gs-spring-boot-docker
+docker run -p 9090:8200 springio/gs-spring-boot-docker

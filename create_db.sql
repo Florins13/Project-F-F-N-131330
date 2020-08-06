5eb4c6435cc16d0b1b9892408ebb1df73dbdbcda
Nuno Ramos <Nuno.Ramos@ctw.bmwgroup.com>
2020-08-06 09:29:28 +0100

    Change UPERCASE to lowercase in sql script

--- create_db.sql
+++ create_db.sql
@@ -1,5 +1,5 @@
-create database db_ffn_TEST;
+create database db_ffn_test;
 
 create user 'ADMIN_FFN'@'%' identified by 'password123';
 
-grant all on db_ffn_TEST.* to 'ADMIN_FFN'@'%';
+grant all on db_ffn_test.* to 'ADMIN_FFN'@'%';

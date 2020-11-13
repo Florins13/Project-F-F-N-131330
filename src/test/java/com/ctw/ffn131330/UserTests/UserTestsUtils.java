1efba9e15a8de4d25f563c9d08f7a7a57f9055e2
Florin Sofinet <Florin.Sofinet@ctw.bmwgroup.com>
2020-11-13 19:39:12 +0000

    added more controller tests

--- src/test/java/com/ctw/ffn131330/UserTests/UserTestsUtils.java
+++ src/test/java/com/ctw/ffn131330/UserTests/UserTestsUtils.java
@@ -3,12 +3,13 @@ package com.ctw.ffn131330.UserTests;
 import com.ctw.ffn131330.user.User;
 
 public class UserTestsUtils {
-
+    String json = "{\n" +
+            "\"name\": \"alaska\"\n" +
+            "}";
     public User mockUser(){
-
-        User usr = new User();
-        usr.setId((long) 1);
-        usr.setName("Florin");
-        return usr;
+        User userOne = new User();
+        userOne.setId(1L);
+        userOne.setName("userone");
+        return userOne;
     }
 }

068ce3ba3b3f89462f9a06567de4d8020d066756
Nuno Ramos <kkadomm@gmail.com>
2021-02-15 11:03:25 +0000

    test routing

--- Front-End/src/app/app-routing.module.ts
+++ Front-End/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {BlueCompComponent} from "./blue-comp/blue-comp.component";
+import {RedCompComponent} from "./red-comp/red-comp.component";
+import {AppComponent} from "./app.component";
+import {PracticeComponent} from "./practice/practice.component";
 
-const routes: Routes = [ { path: 'blue', component: BlueCompComponent }];
+const routes: Routes = [
+  { path: 'red', component: RedCompComponent },
+  { path: 'blue', component: BlueCompComponent },
+  { path: '', component: AppComponent },
+  { path: '**', component: PracticeComponent},
+  { path: '', redirectTo: 'blue', pathMatch: 'full'}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Front-End/src/app/practice/practice.component.ts
+++ Front-End/src/app/practice/practice.component.ts
@@ -1,6 +1,5 @@
 import {Component, Input, OnInit, Output} from '@angular/core';
 import {Users} from "../hero";
-import {HeroService} from "../services/hero.service";
 
 @Component({
   selector: 'app-practice',
@@ -14,34 +13,12 @@ export class PracticeComponent implements OnInit {
   users : Users[] = [];
 
   constructor(
-    private heroService: HeroService,) { }
+    ) { }
 
-  hero: Users = {
-    id: 1,
-    title: 'Windstorm',
-    body: "20",
-    userId: 1
-  };
-
-  HEROES: string[] = [
-    "Dr Nice",
-    "Dr Nice1",
-    "Dr Nice2",
-    "Dr Nice3"
-  ];
 
   ngOnInit(): void {
-    this.name = "init"
   }
 
-  getStuff(): void {
-    this.heroService.getHeroes().subscribe(heroes =>
-    {
-      this.users = heroes
-      console.log(this.users)
-    });
-
-  }
 
 
 }
